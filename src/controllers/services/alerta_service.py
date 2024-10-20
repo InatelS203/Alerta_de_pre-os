a18ccbd485a4b230b2e29680fd8a10e8db785376
GustavoRT-debug <gustavo_ramos@gec.inatel.br>
2024-10-19 21:14:04 -0300

    script para receber e processar as mensagens

--- src/controllers/services/alerta_service.py
+++ src/controllers/services/alerta_service.py
@@ -1,12 +1,30 @@
 #!/usr/bin/env python
 import pika
+import json
 
-connection = pika.BlockingConnection(
-    pika.ConnectionParameters(host='localhost'))
+# Conectar ao RabbitMQ
+connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
 channel = connection.channel()
 
-channel.queue_declare(queue='hello')
+# Declarar a fila 'alertas' no RabbitMQ
+channel.queue_declare(queue='alertas')
 
-channel.basic_publish(exchange='', routing_key='hello', body='Hello World!')
-print(" [x] Sent 'Hello World!'")
+# Exemplo de alerta de preço
+alerta = {
+    "usuario_id": "123",
+    "produto": "Smartphone XYZ",
+    "preco_limite": 1000.0,
+    "data_criacao": "2024-09-02T12:00:00",
+    "status": "ativo"
+}
+
+# Converter o alerta para o formato JSON antes de enviar
+mensagem = json.dumps(alerta)
+
+# Publicar a mensagem na fila 'alertas'
+channel.basic_publish(exchange='', routing_key='alertas', body=mensagem)
+
+print(f" [x] Alerta enviado: {mensagem}")
+
+# Fechar a conexão
 connection.close()
--- src/controllers/services/notificacao_service.py
+++ src/controllers/services/notificacao_service.py
@@ -1,25 +1,30 @@
 #!/usr/bin/env python
-import pika, sys, os
+import pika, sys, os, json
 
 def main():
+    # Conectar ao RabbitMQ
     connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
     channel = connection.channel()
 
-    channel.queue_declare(queue='hello')
+    # Declarar a fila 'alertas'
+    channel.queue_declare(queue='alertas')
 
+    # Função callback que processa a mensagem recebida
     def callback(ch, method, properties, body):
-        print(f" [x] Received {body}")
+        alerta = json.loads(body)  # Converte a mensagem recebida de JSON para um dicionário
+        print(f" [x] Alerta Recebido: Produto: {alerta['produto']}, Preço Limite: {alerta['preco_limite']}, Status: {alerta['status']}")
 
-    channel.basic_consume(queue='hello', on_message_callback=callback, auto_ack=True)
+    # Consumir mensagens da fila 'alertas'
+    channel.basic_consume(queue='alertas', on_message_callback=callback, auto_ack=True)
 
-    print(' [*] Waiting for messages. To exit press CTRL+C')
+    print(' [*] Aguardando mensagens. Para sair, pressione CTRL+C')
     channel.start_consuming()
 
 if __name__ == '__main__':
     try:
         main()
     except KeyboardInterrupt:
-        print('Interrupted')
+        print('Processo interrompido')
         try:
             sys.exit(0)
         except SystemExit:
